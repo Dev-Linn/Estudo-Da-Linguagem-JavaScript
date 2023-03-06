3a6ca509af86d4683cc4f5b9e3c8b1235248e2d9
Linn <lincoln.beitsbancarios.angola@gmail.com>
2023-03-06 16:46:09 -0300

    first commit

---
+++ ARQUIVOS JS/index3.js
@@ -0,0 +1,17 @@
+// let a,b,c; 
+// a=5; b=6; c=7;
+
+let pessoa = "Dimirtri"; //js pode ter espaços em branco, ele aceita quebras de linha.
+
+function minhafunção() {
+
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+
+}
+
+
+
---
+++ ARQUIVOS JS/index4.js
@@ -0,0 +1,20 @@
+console.log(5 + 5) // ele vai somar, vai tetornar = {10}
+console.log(5 + 5) // ele vai ver como um texto, vai retornar = {5+5}
+
+var a = 1;
+var b = 5;
+var c = a + b; // * (vezes (x)) / (divisão) - (subitração)
+console.log(c); // vai retornar = {6}
+
+// O JS é sensivel para letrais maiúscula exemplo:
+
+var PESSOA = "Dimitri" 
+var pessoa = "Teixeira"
+
+console.log(pessoa) //Teixeira
+console.log(PESSOA) //Dimitri
+console.log(PeSsoa) //error
+
+// var primeiro-nome <<< ERRADO!!
+
+

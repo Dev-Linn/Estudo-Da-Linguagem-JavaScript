1ed17c5e2d0fd5b5b4c95001bbb443c77b223ef5
Linn <lincoln.beitsbancarios.angola@gmail.com>
2023-03-06 16:41:12 -0300

    first commit

---
+++ Aula 01 JS Interno e Externo/js/index.js
@@ -0,0 +1,1 @@
+alert('Uma alerta rodando de uma página externa!');
---
+++ Aula 02 - Tipos de Saída/js/index.js
@@ -0,0 +1,3 @@
+document.getElementById ("texto").innerHTML="Meu Primeiro texto <b>JS</b>";
+alert(10 + 5)
+console.log("Oi isso é um console.log")

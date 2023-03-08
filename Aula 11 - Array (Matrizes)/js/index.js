ef0b8e8651c9367fc1a8f1273a8951a06e674f5c
Linn <lincoln.beitsbancarios.angola@gmail.com>
2023-03-07 21:42:00 -0300

    mensagem

---
+++ Aula 11 - Array (Matrizes)/js/index.js
@@ -0,0 +1,13 @@
+/* ARRAYS
+Os arrays JavaScript são usados para armazenar vários valores
+em uma única variável. Diferente dos objetos, que funcionam com
+propriedades ou "nomes" que você dá para os itens dentro dele, os
+arrays não possuem propriedades. O item dentro dele é encontrado pela
+posição.
+Imagine um array como uma lista de itens, com controle, por ordem de
+posição dentro dele.
+Ex: const lista = ["arroz","feijão", "macarrão","leite"];
+A lista[0] (lista na posição 0) vai conter o valor "arroz".
+A lista[1] (lista na posição 1) vai conter o valor "feijão".
+E assim por diante.
+*/
